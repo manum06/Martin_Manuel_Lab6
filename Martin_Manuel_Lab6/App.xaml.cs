2244b985322fbef8627a002c44efabb1c067626d
MANUEL-IONUȚ MARTIN <manuel.martin@stud.ubbcluj.ro>
2022-01-08 17:24:28 +0200

    Add project files.

---
+++ Martin_Manuel_Lab6/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Martin_Manuel_Lab6.Data;
+
+namespace Martin_Manuel_Lab6
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+          
+
+           
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Martin_Manuel_Lab6/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Martin_Manuel_Lab6.Models
+
+{
+    public class ShopList
+    {
+        [PrimaryKey,AutoIncrement]
+        public int ID{get; set;}
+        public string Description {get; set;}
+        public DateTime Date {get; set;}
+    }
+}
